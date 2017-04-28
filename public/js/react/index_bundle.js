c0720c7ade349c45a24be2ab5184bcfc22937d3e
Safanar <satan251008@gmail.com>
2017-04-28 21:57:58 +0800

    Update index_bundle.js

--- public/js/react/index_bundle.js
+++ public/js/react/index_bundle.js
@@ -760,7 +760,7 @@ var Map=function(){
 					for(var i=0;i<bikes.length;i++){
 						var obj=new _bike2.default(this,bikes[i],i);
 						obj.attachSecretMessage();
-						this.rubikes.push(null)
+						this.rubikes.push(obj)
 					}
 				else for(var i=0;i<bikes.length&&i<this.rubikes.length;i++){var bike=bikes[i];var myLatlng=new google.maps.LatLng(parseFloat(bike.location.latitude),parseFloat(bike.location.longitude));this.rubikes[i].marker.setPosition(myLatlng)}
 				}
