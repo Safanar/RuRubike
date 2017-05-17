2ec1f749e32576798ad85b98cdee0cd79d467eac
Safanar <satan251008@gmail.com>
2017-05-17 10:14:30 +0800

    Update rubikeAPI.js

--- class/api/rubikeAPI.js
+++ class/api/rubikeAPI.js
@@ -1,5 +1,6 @@
 var Login = require('./login/login.js');
 var ViewOfBikeInfo = require('./viewOfBikeInfo/viewOfBikeInfo.js');
+var ViewOfCarInfo = require('./viewOfCarInfo/viewOfCarInfo.js');
 var Contact = require('./contact/contact.js');
 var Bike = require('./bike/bike.js');
 var Car = require('./bike/car.js');
@@ -13,6 +14,7 @@ class ruruBikeApi
 		this.bike = new Bike(app,mongodb);
 		this.car = new Car(app,mongodb);
 		this.viewOfBikeInfo = new ViewOfBikeInfo(app,mongodb);
+		this.viewOfCarInfo = new ViewOfCarInfo(app,mongodb);
 		this.contact = new Contact(app,mongodb);
 		this.apiAccess = {};
 		this.apiAccess.login = function(id,password,callback) 
